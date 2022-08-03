4f23467424c0e48a9315a27f132135a94a352cef
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-08-03 14:28:28 +0200

    zad2 cd

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,8 +1,11 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
 const timer = document.querySelector(".timer");
 const value = Array.from(document.querySelectorAll("span.value"));
 const label = Array.from(document.querySelectorAll(".label"));
 const field = Array.from(document.querySelectorAll("div.field"));
-const input = document.querySelector("#datetime-picker");
+const input = document.querySelector("input#datetime-picker");
 const startBtn = document.querySelector(`button[data-start]`);
 
 timer.style.display = "flex";
@@ -25,6 +28,30 @@ for (const labels of label) {
     labels.style.lineHeight = "0.2";    
 };
 
+startBtn.disabled = true;
 
 
-console.log(startBtn);
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+    const todayDate = new Date();
+    if (selectedDates[0].getTime() <= todayDate.getTime()) {
+        startBtn.disabled = true;
+        window.alert("Please choose a date in the future");
+    };
+    if (selectedDates[0].getTime() > todayDate.getTime()) {
+        startBtn.disabled = false;
+        console.log(selectedDates[0]);
+    };    
+  },
+};
+
+flatpickr(input, options);
+
+
+
+const date = new Date();
+console.log(date.getTime());
