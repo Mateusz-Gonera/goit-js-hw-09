779669d63253965be801a22ba7c48f32f44ffb1e
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-08-05 14:15:31 +0200

    kosmetyka 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,14 +1,15 @@
-const body = document.querySelector("body");
-const btnStart = document.querySelector("button[data-start]");
-const btnStop = document.querySelector("button[data-stop]");
+const body = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
-btnStart.style.width = "150px";
-btnStart.style.height = "50px";
-btnStart.style.fontSize = "20px";
 
-btnStop.style.width = "150px";
-btnStop.style.height = "50px";
-btnStop.style.fontSize = "20px";
+btnStart.style.width = '150px';
+btnStart.style.height = '50px';
+btnStart.style.fontSize = '20px';
+
+btnStop.style.width = '150px';
+btnStop.style.height = '50px';
+btnStop.style.fontSize = '20px';
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -17,17 +18,17 @@ function getRandomHexColor() {
 let bgChange = null;
 
 const colorChanger = () => {
-   return body.style.backgroundColor = `${getRandomHexColor()}`
+  return (body.style.backgroundColor = `${getRandomHexColor()}`);
 };
 
 const colorStop = () => {
-    clearInterval(bgChange);
-    btnStart.disabled = false;
+  clearInterval(bgChange);
+  btnStart.disabled = false;
 };
 
-btnStart.addEventListener("click", () => {
-    bgChange = setInterval(colorChanger, 1000);
-    btnStart.disabled = true;
+btnStart.addEventListener('click', () => {
+  bgChange = setInterval(colorChanger, 1000);
+  btnStart.disabled = true;
 });
 
-btnStop.addEventListener("click", colorStop);
+btnStop.addEventListener('click', colorStop);
